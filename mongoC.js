d1312bf0ea21e7c4c6fe093f642473ddd139f64e
Hatem-Dridi <hatem@MyComputer.>
2024-10-15 21:52:06 +0100

    Deploy to ec2

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://hatem1396:${password}@devcluster.rrghw.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://dridihatem1396:${password}@devcluster.rrghw.mongodb.net/?retryWrites=true&w=majority`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
@@ -10,5 +10,5 @@ try {
 } catch(e) {
   console.error(e);
 }
-let db = conn.db("hatem1396");
+let db = conn.db("dridihatem1396");
 export default db;
