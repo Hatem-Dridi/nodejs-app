787fcada0a68a91486d5f12be5d54bebe1aa436b
Hatem-Dridi <hatem@MyComputer.>
2024-10-16 11:26:10 +0100

    change mongoC.js

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://dridihatem1396:${password}@devcluster.rrghw.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://dridihatem1396:${password}@devcluster.7ijp8.mongodb.net/?retryWrites=true&w=majority&appName=devcluster`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
